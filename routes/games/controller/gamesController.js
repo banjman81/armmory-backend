a303afff6faf7f18d4db550d296c05b8f718cca2
[banjman81] <benjamin.man97@gmail.com>
2021-12-06 14:43:38 -0600

    remove game

--- routes/games/controller/gamesController.js
+++ routes/games/controller/gamesController.js
@@ -30,7 +30,47 @@ async function createGame(req, res){
     }
 }
 
+async function removeGame(req, res){
+    try{
+
+        const removedGame = await Game.find({gameId: req.params.gameId})
+        console.log(removedGame.length, '-------xx-----')
+        if(removedGame.length < 1){
+            res.status(500).json({
+                message: "error",
+                error: "Not found"
+            })
+
+        }else{
+            console.log("44 game controller")
+            const foundUser = await User.findOne({email: req.user.email})
+    
+            const deletedGame = await Game.findByIdAndDelete(removedGame[0]._id)
+    
+            let filteredGames = foundUser.favoriteGames.filter(
+                item => item._id.toString() !== deletedGame._id.toString()
+            )
+    
+            foundUser.favoriteGames = filteredGames
+            await foundUser.save()
+    
+            res.json({
+                message: "success",
+                payload1 : deletedGame,
+                payload2: foundUser
+            })
+        }
+
+    }catch(err){
+        res.status(500).json({
+            message: "error",
+            error: err.message
+        })
+    }
+}
+
 
 module.exports = {
-    createGame
+    createGame,
+    removeGame
 }
--- routes/games/gamesRouter.js
+++ routes/games/gamesRouter.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const passport = require("passport")
 
-const { createGame } = require('./controller/gamesController')
+const { createGame, removeGame } = require('./controller/gamesController')
 
 router.post('/add-game', passport.authenticate("listingUser", {session : false}), createGame)
+router.delete('/delete-game/:gameId', passport.authenticate("listingUser", {session : false}), removeGame)
 
 module.exports = router
