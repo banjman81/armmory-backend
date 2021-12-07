288d4110defc3eb2a5eb70d75aac69d9e2a1883d
[banjman81] <benjamin.man97@gmail.com>
2021-12-07 14:45:58 -0600

    backend validation

--- routes/utils/error/ErrorClass.js
+++ routes/utils/error/ErrorClass.js
@@ -45,6 +45,12 @@ function handleMongoServerValidationError(error){
                 errObj.errorInfo = "You exceeded the maximum number";
                 break;
 
+            case "mix":
+                errObj.statusCode = 400;
+                errObj.message = mongoErrorObj[errName].properties.message;
+                errObj.errorInfo = "You need to be above the minimum number";
+                break;
+
             case "regexp":
                 errObj.statusCode = 500;
                 errObj.message = mongoErrorObj[errName].properties.message;
